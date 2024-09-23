195c409bbb068b87c91260cee3fa2573900c8cfa
Anvar1234 <kingartaved@yandex.ru>
2024-09-24 01:01:41 +0500

    Созданы пакеты сервис и контроллер и добавлена логика, добавлены утильные классы, все стратегии собраны в одном утильном классе, созданы соответствующие мапы. И еще немножко почистил.

--- src/main/java/org/example/project/AppRunner.java
+++ src/main/java/org/example/project/AppRunner.java
@@ -2,8 +2,6 @@ package org.example.project;
 
 import org.example.project.controller.AppController;
 
-import java.io.IOException;
-
 public class AppRunner {
     public static void main(String[] args) {
         AppController.run();
